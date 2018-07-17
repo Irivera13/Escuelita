ae10e2b60b7112bcbff53e230239cc4989fce9ce
VALE\Ingrid Rivera <ivrc_s_95@hotmail.com>
2018-07-17 00:42:44 -0600

    Base

---
+++ SAMHS/SAMHS.DATA/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAMHS.DATA
+{
+    public class Class1
+    {
+    }
+}
---
+++ SAMHS/SAMHS.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAMHS.UI.MvcApplication" Language="C#" %>
