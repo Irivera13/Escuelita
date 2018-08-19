72afdf0228d4509f5025c2b6992302aafb93bf68
VALE\Ingrid Rivera <ivrc_s_95@hotmail.com>
2018-08-19 14:21:50 -0600

    Calendario

---
+++ SAMHS/SAMHS.DAL/Metodos/MCalendario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAMHS.DATA;
+using SAMHS.DAL.Interfaces;
+using ServiceStack.OrmLite;
+
+namespace SAMHS.DAL.Metodos
+{
+    public class MCalendario : MBase, ICalendario
+    {
+        public List<Calendario> ListaCalendario()
+        {
+            return _db.Select<Calendario>();
+        }
+    }
+}
---
+++ SAMHS/SAMHS.UI/Models/Calendario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAMHS.UI.Models
+{
+    public class Calendario
+    {
+
+        public int EventID { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? end { get; set; }
+
+        public string ThemeColor { get; set; }
+
+        public bool IsFullDay { get; set; }
+
+    }
+}
